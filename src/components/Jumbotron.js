2732ae79d321ca262611c01d065560ec71c1a8c1
David John <david.john@mail.utoronto.ca>
2023-08-24 12:23:39 -0400

    Updated Jumbotron styling

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,22 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import "./Jumbotron.css";
 
 function Jumbotron (props) {
     return (
         <Container className="jumbotron">
-            <Card className="jumboCard">
-                <Card.Header as="h5">Featured</Card.Header>
-                <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
-                    <Card.Text>
-                    With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+            <Row>
+                <Col xs={7}>
+                    <Card className="jumboCard">
+                        <Card.Body>
+                            <Card.Title>Special title treatment</Card.Title>
+                            <Card.Text>
+                            With supporting text below as a natural lead-in to additional content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col></Col>
+            </Row>
         </Container>
     );
 }
