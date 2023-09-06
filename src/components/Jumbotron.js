e525a3a12c6ff52ae36163dd1fbbd45f7d2fca96
David John <david.john@mail.utoronto.ca>
2023-09-06 14:00:10 -0400

    Refactored jumbotron for readability

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,24 +6,46 @@ import Col from 'react-bootstrap/Col';
 
 import "./Jumbotron.css";
 
-function Jumbotron (props) {
+/* Compartmentalizing text for readability */
+
+/* Wrapper for Jumbotron to contain card */
+let JumbotronWrapper = (props) => {
     return (
         <Container className="jumbotron" style={ {backgroundImage: "url(" + props.JumImg + ")"}}>
             <Row>
-                <Col xs={7}>
-                    <Card className="jumboCard">
-                        <Card.Body>
-                            <Card.Title>{props.CardTle}</Card.Title>
-                            <Card.Text>
-                            {props.CardTxt}
-                            </Card.Text>
-                            <Button variant="primary">{props.BtnTxt}</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                <Col xs={7}>{props.children}</Col>
                 <Col></Col> {/* Space added for this column */}
             </Row>
         </Container>
+    );
+}
+
+/* Jumbotron card */
+let JumbotronCard = (props) => {
+    return (
+        <Card className="jumboCard">
+            <Card.Body>
+                <Card.Title>{props.CardTle}</Card.Title>
+                <Card.Text>
+                {props.CardTxt}
+                </Card.Text>
+                <Button variant="primary">{props.BtnTxt}</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+function Jumbotron (props) {
+     
+    return (
+        <JumbotronWrapper JumImg={props.JumImg}>
+            <JumbotronCard 
+                CardTle={props.CardTle}
+                CardTxt={props.CardTxt}
+                BtnTxt={props.BtnTxt}
+                />
+        </JumbotronWrapper>
+        
     );
 }
 
