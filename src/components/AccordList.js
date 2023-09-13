9bdd786663aed71712cd81ca47d73b1568c08a7d
David John <david.john@mail.utoronto.ca>
2023-09-13 17:46:52 -0400

    Figuring out styling of the accordList

--- src/components/AccordList.js
+++ src/components/AccordList.js
@@ -5,15 +5,18 @@ import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge'
 
 
 // Using function from Bootstrap React
 function CustomToggle({ children, eventKey }) {
     const decoratedOnClick = useAccordionButton(eventKey);
     return (
-      <Button variant="light" onClick={decoratedOnClick}>
-        {children}
-      </Button>
+        <Badge bg="light" pill>
+            <Button variant="outline-dark" onClick={decoratedOnClick}>
+                {children}
+            </Button>
+        </Badge>
     );
 }
 
@@ -31,16 +34,20 @@ function AccordList(props) {
     // Using name and list from json
     return (
         <>
-            <Row>
-                <Col>
-                    {props.name}
-                </Col>
-                <Col>
-                    <CustomToggle eventKey={props.num}>
-                        +
-                    </CustomToggle>
-                </Col>
-            </Row>
+            <Card>
+                <Row>
+                    <Col className="text-center">
+                        <Button variant="outline-primary" size="lg" disabled>
+                            {props.name}
+                        </Button>
+                    </Col>
+                    <Col>
+                        <CustomToggle eventKey={props.num}>
+                            +
+                        </CustomToggle>
+                    </Col>
+                </Row>
+            </Card>
             <Accordion.Collapse eventKey={props.num}>
                 <Card style={{textAlign : "left"}}>
                     <ListGroup defaultActiveKey={props.list[0].link}>
