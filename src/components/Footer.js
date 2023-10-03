c4a58507b69c3130e3d519e116c77ffc886dfe94
David John <david.john@mail.utoronto.ca>
2023-10-03 13:34:57 -0400

    Currently using listgroup for links list

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 import './Footer.css';
 
 function Footer() {
@@ -8,15 +9,56 @@ function Footer() {
         <div className="bottomBox">
             <Container fluid className="bottomDiv">
                 <Row>
-
+                    <Col>
+                        <h6>About</h6>
+                        <ListGroup>
+                            <ListGroup.Item variant="dark" action href="#link1">
+                                Link 1
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="#link2">
+                                Link 2
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="">
+                                Link 3
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col>
+                        <h6>About</h6>
+                        <ListGroup>
+                            <ListGroup.Item action href="#link1">
+                                Link 1
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="#link2">
+                                Link 2
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="">
+                                Link 3
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col>
+                        <h6>About</h6>
+                        <ListGroup>
+                            <ListGroup.Item action href="#link1">
+                                Link 1
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="#link2">
+                                Link 2
+                            </ListGroup.Item>
+                            <ListGroup.Item action href="">
+                                Link 3
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Col>
                 </Row>
                 <Row>
 
                 </Row>
                 <Row>
-                    <Col></Col>
                     <Col></Col>
                     <Col>© 2023 David John.</Col>
+                    <Col></Col>
                 </Row>
             </Container>
         </div>
