453d3222de8483b7953c89b19cc9b1efb913a27b
David John <david.john@mail.utoronto.ca>
2023-11-09 15:21:12 -0500

    Updated formatting of tabs for Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,7 @@ import CarouselCards from './CarouselCards';
 import AccordianGroup from './AccordianGroup';
 import ACCORDJSON from '../Accordian.json';
 
-
 function Home() {
-  // console.log(ACCORDJSON);
-  //let accordjson = require('../Accordian.json');
-  //{Math.floor(Math.random() * (200 - 1) + 0)}
-  // let randomPic = "https://picsum.photos/id/" +Math.floor(Math.random() * (200 - 1) + 0) +"/200/300";
   return (
     <Container className="Page" fluid>
       <Jumbotron
